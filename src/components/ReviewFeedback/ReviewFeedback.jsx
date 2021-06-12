3d790e85d4a2ab65e6956debd42c60dd0d0c2c71
Terry O'Keefe <terryokeefe@Mac-Mini.local>
2021-06-12 10:00:05 -0500

    submit button sends user to submission route

--- src/components/ReviewFeedback/ReviewFeedback.jsx
+++ src/components/ReviewFeedback/ReviewFeedback.jsx
@@ -1,4 +1,5 @@
 import {useSelector, useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 import axios from 'axios';
 import './ReviewFeedback.css';
 
@@ -6,9 +7,17 @@ import './ReviewFeedback.css';
 function ReviewFeedback() {
     // import data from store
     const reviewList = useSelector(store => store.feedbackReducer)
-    // console log
-    console.log(reviewList[reviewList.feeling]);
 
+    // variable for useHistory
+    const history = useHistory();
+
+    // axios to send review to database
+    const reviewSubmit = () => {
+
+
+        // on click change to submission route
+        history.push('/submission');
+    }
     
     return (
         <div>
@@ -16,11 +25,11 @@ function ReviewFeedback() {
                 <h2>Review Your Feedback</h2>
             </div>
             <div>
-                <h3>Feelings: {reviewList[0].feeling}</h3>
+                {/* <h3>Feelings: {reviewList[0].feeling}</h3>
                 <h3>Understanding: {reviewList[1].understanding}</h3>
                 <h3>Support: {reviewList[2].support}</h3>
-                <h3>Comments: {reviewList[3].comments}</h3>
-                <button>Submit</button>
+                <h3>Comments: {reviewList[3].comments}</h3> */}
+                <button onClick={reviewSubmit}>Submit</button>
             </div>
         </div>
     )
