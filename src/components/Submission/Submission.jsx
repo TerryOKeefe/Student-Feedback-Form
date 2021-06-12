bbb950041e2ad384d5907e125b67db6ebaf1f6f8
Terry O'Keefe <terryokeefe@Mac-Mini.local>
2021-06-12 10:15:49 -0500

    submission component setup

--- src/components/Submission/Submission.jsx
+++ src/components/Submission/Submission.jsx
@@ -1,4 +1,23 @@
+import {useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+import './Submission.css';
+
+// function for Submission component
 function Submission() {
+
+    // variables for dispatch and UseHistory
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleSubmit = () => {
+        // dispatch to clear data
+        dispatch({
+            type : 'SUBMISSION'
+        });
+
+        // on click change to home route
+        history.push('/')
+    }
     return (
         <div>
             <div>
@@ -6,10 +25,11 @@ function Submission() {
             </div>
             <div>
                 <h3>Thank You!</h3>
-                <button>Leave New Feedback</button>
+                <button onClick={handleSubmit}>Leave New Feedback</button>
             </div>
         </div>
     )
-}
+} // end Submission
 
+// export Submission
 export default Submission;
